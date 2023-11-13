2fa3caa06f60b3249c10631f5e62c537ab629ab2
samie54 <126026530+samie54@users.noreply.github.com>
2023-11-13 10:38:51 -0800

    Task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,4 @@
-# stack fix to get zero (0) errors.
+# Stack fix to get zero (0) errors.
 exec { 'file limit':
   onlyif   => 'test -e /etc/default/nginx',
   command  => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx; service nginx restart',
